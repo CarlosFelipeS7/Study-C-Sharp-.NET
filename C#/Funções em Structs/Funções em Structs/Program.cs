101d25af3bc5e3989f3a05c49d824e22f075fbdb
Carlos Felipe Santos <ccfelipe08@gmail.com>
2025-08-15 16:21:12 -0300

    Add multiple C# project samples and solutions
    This commit introduces several C# sample projects, each with its own solution, source files, configuration, and build outputs. The projects cover topics such as structs, arrays, conditional structures, constructors, enums, switch statements, loops, and functions, providing a comprehensive set of examples for learning and reference.

---
+++ C#/Funções em Structs/Funções em Structs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funções_em_Structs
+{
+    internal class Program
+    {
+        //struct
+        struct Produto
+        {
+            public string nome;
+            public float preco;
+            public float peso;
+            public string marca;
+
+            //constructor
+            public Produto(string nome, float preco, float peso, string marca)
+            {
+                this.nome = nome;
+                this.preco = preco;
+                this.peso = peso;
+                this.marca = marca;
+            }
+
+            void 
+
+        }
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
