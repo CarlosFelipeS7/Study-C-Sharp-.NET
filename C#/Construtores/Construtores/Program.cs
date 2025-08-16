ff3ced6812033ab91092d85b5214758481026195
Carlos Felipe Santos <ccfelipe08@gmail.com>
2025-08-16 17:36:05 -0300

    Testando 2025

--- C#/Construtores/Construtores/Program.cs
+++ C#/Construtores/Construtores/Program.cs
@@ -42,7 +42,7 @@ namespace Construtores
             Console.WriteLine(bola.peso);
             Console.WriteLine(bola.marca);
 
-            Console.WriteLine("Atualizado");
+            Console.WriteLine("Atualizado 2025");
             Console.ReadLine();
         }
     }
